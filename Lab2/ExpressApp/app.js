59a1931378aff1d1731297e8d15cb342453eda9d
DanielC000 <91308079+DanielC000@users.noreply.github.com>
2022-11-03 23:05:38 +0100

    Update app.js

--- Lab2/ExpressApp/app.js
+++ Lab2/ExpressApp/app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const appInsights = require("applicationinsights");
+const { Console } = require("console");
 
 var app = express();
 
@@ -45,4 +46,18 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-module.exports = app;
+app.set("port", process.env.PORT || 3000);
+
+exports.listen = function () {
+  server = app.listen(app.get("port"), function () {
+    debug("Express server listening on port " + server.address().port);
+  });
+};
+
+exports.close = function () {
+  server.close(() => {
+    debug("Server stopped.");
+  });
+};
+console.log("Running");
+this.listen();
